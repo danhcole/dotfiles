c40d08f8cd2d610792f56b31ee114d9db2466012
Daniel Cole <daniel.cole@mongodb.com>
2018-08-01 16:12:09 -0400

    Update vimrc

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -8,3 +8,16 @@ bindkey "[A"      history-search-backward              # up arrow
 bindkey "^D"      delete-char                          # ctrl-d
 bindkey "^F"      forward-char                         # ctrl-f
 bindkey "^B"      backward-char                        # ctrl-b
+
+function ranger-cd {
+    tempfile="$(mktemp -t tmp.XXXXXX)"
+    /usr/local/bin/ranger --choosedir="$tempfile" "${@:-$(pwd)}"
+    test -f "$tempfile" &&
+    if [ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]; then
+        cd -- "$(cat "$tempfile")"
+    fi
+    rm -f -- "$tempfile"
+}
+
+
+bindkey -s '^O' 'ranger-cd\n'
